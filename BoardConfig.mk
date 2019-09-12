24a5f057630f9ad7ddc9035d4138ef15dba9d775
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-13 00:17:36 +0200

    begonia: Dexpreopt Everything
    Change-Id: I65e8ce61e33256b2f2beecf390f6926b70920d02

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,6 @@ TARGET_NO_BOOTLOADER := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     WITH_DEXPREOPT ?= true
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
   endif
 endif
 
